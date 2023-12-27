1afa39d286365cea35a75fe71ae858ff8c3eb73b
arh91 <alvaro_rh_991@hotmail.com>
2023-12-28 00:13:05 +0100

    Corrijo algunos errores... Al cambiar los nombres de los atributos de entidades también tengo que reflejar esos cambios en las sentencias sql en la clase @Dao.

--- app/src/main/java/com/example/agenda/AgendaDatabase.kt
+++ app/src/main/java/com/example/agenda/AgendaDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Supplier::class, Supplier::class], version = 1)
+@Database(entities = [Customer::class, Supplier::class], version = 1)
 abstract class AgendaDatabase : RoomDatabase() {
     abstract fun peticionesDao(): PeticionesDao
 
--- app/src/main/java/com/example/agenda/PeticionesDao.kt
+++ app/src/main/java/com/example/agenda/PeticionesDao.kt
@@ -1,9 +1,11 @@
 package com.example.agenda
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 
+@Dao
 interface PeticionesDao {
 
     @Insert
@@ -12,13 +14,13 @@ interface PeticionesDao {
     @Query("SELECT * FROM Customer")
     fun getAllCustomers(): List<Customer>
 
-    @Query("SELECT * FROM Customer WHERE codigo = :customerId")
+    @Query("SELECT * FROM Customer WHERE codigoCli = :customerId")
     suspend fun getCustomerById(customerId: String): Customer
 
-    @Query("DELETE FROM Customer WHERE codigo = :customerId")
+    @Query("DELETE FROM Customer WHERE codigoCli = :customerId")
     suspend fun deleteCustomerById(customerId: String)
 
-    @Query("UPDATE Customer SET nombre = :customerName, direccion = :customerAddress, telefono = :customerPhone WHERE codigo = :customerId")
+    @Query("UPDATE Customer SET nombreCli = :customerName, direccionCli = :customerAddress, telefonoCli = :customerPhone WHERE codigoCli = :customerId")
     suspend fun updateCustomerById(customerId: String, customerName: String, customerAddress: String, customerPhone: String)
 
     @Insert
@@ -27,21 +29,21 @@ interface PeticionesDao {
     @Query("SELECT * FROM Supplier")
     fun getAllSuppliers(): List<Supplier>
 
-    @Query("SELECT * FROM Supplier WHERE codigo = :supplierId")
+    @Query("SELECT * FROM Supplier WHERE codigoProv = :supplierId")
     suspend fun getSupplierById(supplierId: String): Supplier
 
-    @Query("DELETE FROM Supplier WHERE codigo = :supplierId")
+    @Query("DELETE FROM Supplier WHERE codigoProv = :supplierId")
     suspend fun deleteSupplierById(supplierId: String)
 
-    @Query("UPDATE Supplier SET nombre = :supplierName, direccion = :supplierAddress, telefono = :supplierPhone WHERE codigo = :supplierId")
+    @Query("UPDATE Supplier SET nombreProv = :supplierName, direccionProv = :supplierAddress, telefonoProv = :supplierPhone WHERE codigoProv = :supplierId")
     suspend fun updateSupplierById(supplierId: String, supplierName: String, supplierAddress: String, supplierPhone: String)
 
 
     //Comprobamos si el código que recibe como parámetro existe en la entidad Customer
-    @Query("SELECT EXISTS(SELECT 1 FROM Customer WHERE codigo = :customerId LIMIT 1)")
+    @Query("SELECT EXISTS(SELECT 1 FROM Customer WHERE codigoCli = :customerId LIMIT 1)")
     fun isCodigoCustomerExists(customerId: String): LiveData<Boolean>
 
     //Comprobamos si el código que recibe como parámetro existe en la entidad Supplier
-    @Query("SELECT EXISTS(SELECT 1 FROM Supplier WHERE codigo = :supplierId LIMIT 1)")
+    @Query("SELECT EXISTS(SELECT 1 FROM Supplier WHERE codigoProv = :supplierId LIMIT 1)")
     fun isCodigoSupplierExists(supplierId: String): LiveData<Boolean>
 }
